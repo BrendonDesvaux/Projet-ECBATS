ab9069e27e7f1fa0c0dc49f22ffbdfa78e1cb860
EloiPriol <eloi.priol@algosup.com>
2021-10-29 11:09:20 +0200

    Adding Arbitrage
    Adding Arbitrage + Remove bug

---
+++ ECBATS/ECBATS/API/ArbitrageCall.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Web;
+namespace ECBATS.API
+{
+    public class ArbitrageCall
+    {
+        public static (List<List<double>>, List<List<string>>) ArbitrageCalculus()
+        {
+            string strpercentage = CoinGeckoArbitrageCall.makeArbitrageCall().strprc;
+            string strprice = CoinGeckoArbitrageCall.makeArbitrageCall().strprice;
+
+            List<string> CoinGeckoList = new List<string> { strprice };
+            List<string> CryptoCompareList = new List<string> { CryptoCompareArbitrageCall.CryptoCompareAPIcall.makeAPICall() };
+
+            List<List<string>> arbitrage = new List<List<string>> { CryptoCompareList, CoinGeckoList };
+
+            double CryptoCompare = double.Parse(CryptoCompareList[0]);
+            double ArbitragePercent = double.Parse(strpercentage);
+
+            double CoinGecko;
+
+            try
+            {
+                CoinGecko = double.Parse("55555");
+            }
+            catch (FormatException) { }
+
+            CoinGecko = double.Parse(CoinGeckoList[0]);
+
+            if (CryptoCompare >= CoinGecko)
+            {
+                arbitrage.Remove(CryptoCompareList);
+                arbitrage.Remove(CoinGeckoList);
+                arbitrage = new List<List<string>> { CoinGeckoList, CryptoCompareList };
+            }
+            CryptoCompare = CryptoCompare + CryptoCompare * ArbitragePercent / 100;
+            CoinGecko = CoinGecko + CoinGecko * ArbitragePercent / 100;
+
+            List<double> CryptoCompareList24h = new List<double> { CryptoCompare };
+            List<double> CoinGeckoList24h = new List<double> { CoinGecko };
+
+            List<List<double>> arbitrage24h = new List<List<double>> { CryptoCompareList24h, CoinGeckoList24h };
+
+            if (CryptoCompare >= CoinGecko)
+            {
+                arbitrage24h.Remove(CryptoCompareList24h);
+                arbitrage24h.Remove(CoinGeckoList24h);
+                arbitrage24h = new List<List<double>> { CoinGeckoList24h, CryptoCompareList24h };
+            }
+            return (arbitrage24h, arbitrage);
+        }
+    }
+}
+
--- ECBATS/ECBATS/API/CoinGeckoArbitrageCall.cs
+++ ECBATS/ECBATS/API/CoinGeckoArbitrageCall.cs
@@ -3,146 +3,127 @@ using System.Collections.Generic;
 using System.Net;
 using System.Web;
 using CoinGecko;
-
-
-namespace GeckoAPICall
-{ 
-    public class GeckoCALL
+namespace ECBATS.API
+{
+    public class CoinGeckoArbitrageCall
     {
+        
         public static string setURL = " https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_rank_desc&per_page=33&page=1&sparkline=false&price_change_percentage=24h%2C7d";
-        public static List<string> makeAPICall()
+        public static (string strprice, string strprc) makeArbitrageCall(string Symbol) // return values in euro of current price & percent price  24h
         {
+
+            string symbolFav = Symbol;
+        string strprice = "";
+            string strprc = "";
             List<string> strList = new List<string>();
             var client = new WebClient();
             int y = 0;
-            var upDateURL = "";
+            string upDateURL;
             for (int i = 0; i < 231; i += 33)// call SwapString for call 231 Crypto
             {
                 y += 33;
                 string str = y.ToString();
                 SwapString(str);
                 upDateURL = SwapString(str);
-            }
+                var URL = new UriBuilder(upDateURL);
 
-            var URL = new UriBuilder(upDateURL);
+                client.Headers.Add("Accepts", "application/json");
 
-            client.Headers.Add("Accepts", "application/json");
+                string[] shot = client.DownloadString(URL.ToString()).Split('{', ',', '[', '}', ']');
 
-            Console.Write(client.DownloadString(URL.ToString()));
-
-            string[] shot = client.DownloadString(URL.ToString()).Split('{', ',', '[','}');
-
-            foreach (var sub in shot)
-            {
-                if (sub.Length < 1)
+                foreach (var sub in shot)
                 {
-
-                    continue;
+                    if (sub.Length <= 1)
+                    {
+                        continue;
+                    }
+                    if (sub[1] == 'p' && sub.Length > 45 || sub[1] == 's' && sub[2] == 'y' || sub[1] == 'c' && sub[7] == 't') //ne pas effacer
+                    {
+                        strList.Add(sub);
+                    }
                 }
-                if (sub[1] == 'i' && sub[2] == 'm' ||  sub[1] == 'p' && sub.Length > 45 || sub[1] == 'i' && sub[2] == 'd' || sub[1] == 'n' || sub[1] == 's' && sub[2] == 'y' || sub[1] == 'c' && sub[7] == 't')
-                {
-                    strList.Add(sub);
-                }
-
             }
             var x = strList.Count;
-            for (var i = 0; i < x; i++)
+            for (var z = 0; z < x; z++)
             {
                 var length = 0;
-                for (int ii = 1; ii < strList[i].Length; ii++)
-                    if (strList[i][ii] == '"')
+                for (int ii = 1; ii < strList[z].Length; ii++)
+                    if (strList[z][ii] == '"')
                     {
                         length = ii;
                         break;
                     }
+                strList[z] = strList[z].Remove(0, length + 2);
 
-                strList[i] = strList[i].Remove(0, length + 2);
-                if (strList[i][0] == '"')
+                if (strList[z][0] == '"')
                 {
-                    strList[i] = strList[i].Remove(0, 1);
-                    strList[i] = strList[i].Remove(strList[i].Length - 1, 1);
+
+                    strList[z] = strList[z].Remove(0, 1);
+
+                    strList[z] = strList[z].Remove(strList[z].Length - 1, 1);
+
                 }
 
-                Console.WriteLine(strList[i]);
             }
 
+            for (var zz = 0; zz < x; zz++)
+            {
+                if (String.Compare(strList[zz], symbolFav) == 0)
+                {
+                    strprice = strList[zz + 1];
+                    strprc = strList[zz + 2];
+                    return (strprice, strprc);
+                }
+            }
 
-            return (strList);
-
+            return (strprice, strprc);
         }
         public static string SwapString(string str)
         {
-            string sampleURL = setURL; //"https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_rank_desc&per_page=33&page=1&sparkline=false&price_change_percentage=24h%2C7d"; // url want to change
-            //Console.WriteLine(sampleURL);
+            string sampleURL = setURL;
 
             string find = "_page="; //consistant string to find the numbers you want to change 
-
-
-
 
             var first_len = 0;
             var second_len = 0;
             var trig = 0;
-
-
 
             for (int i = 0; i < sampleURL.Length; i++) // first loop for find location BEFORE number need to remplace
             {
                 if (i == sampleURL.IndexOf("_page="))
                 {
                     i += find.Length;
-
                     first_len += i;
-
                 }
-
-
             }
             for (int i = 0; i < sampleURL.Length; i++) // first loop for find location AFTER number need to remplace
             {
                 if (i == sampleURL.IndexOf("&page="))
                 {
-
-
                     second_len += i;
-
                 }
-
             }
             var empty_space = second_len - first_len;
 
             var ch_to_str = "";
             string replacement;
 
-
-
-
             for (int i = 0; i < sampleURL.Length; i++)// replace the string with str argument and return the new URL
             {
-
                 if (i >= first_len && i < second_len)
                 {
                     ch_to_str += sampleURL[i].ToString();
                     trig++;
-
-
-
 
                     if (trig == empty_space)
                     {
                         replacement = sampleURL.Replace(ch_to_str, str);
                         //Console.WriteLine(replacement);
                         return replacement;
-
                     }
-
                 }
-
             }
-
             return sampleURL;
         }
-
-
     }
 }
